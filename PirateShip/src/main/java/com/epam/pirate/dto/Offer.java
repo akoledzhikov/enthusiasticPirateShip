1de543e0a82de7fa80708a25b8c6e65d06ac2b39
Alex <night_guard@abv.bg>
2016-10-06 09:25:29 +0300

    Target charity now part of the Offer DTO.

--- PirateShip/src/main/java/com/epam/pirate/dto/Offer.java
+++ PirateShip/src/main/java/com/epam/pirate/dto/Offer.java
@@ -3,6 +3,8 @@ package com.epam.pirate.dto;
 
 import java.util.ArrayList;
 
+import com.epam.pirate.model.Charity;
+
 
 public class Offer
 {
@@ -14,25 +16,32 @@ public class Offer
     private String description;
 
     private ArrayList<String> pictures = new ArrayList<>();
-    
+
     private String firstImage;
 
     private double price;
-    
-	public Offer(com.epam.pirate.model.Offer offer){
-    	id = offer.getId();
-    	title = offer.getTitle();
-    	category = offer.getCategory();
-    	description = offer.getDescription();
-    	pictures = offer.getPictures();
-    	price = offer.getPrice();
-    	setFirstImage(pictures.isEmpty() ? null : pictures.get(0));
+
+    private TargetCharity charity;
+
+
+    public Offer(com.epam.pirate.model.Offer offer)
+    {
+        id = offer.getId();
+        title = offer.getTitle();
+        category = offer.getCategory();
+        description = offer.getDescription();
+        pictures = offer.getPictures();
+        price = offer.getPrice();
+        setFirstImage(pictures.isEmpty() ? null : pictures.get(0));
+        setCharity(new TargetCharity(offer.getTargetCharity()));
     }
-    
+
+
     public long getId()
     {
         return id;
     }
+
 
     public void setId(long id)
     {
@@ -99,14 +108,74 @@ public class Offer
         this.price = price;
     }
 
+
     public String getFirstImage()
     {
         return firstImage;
     }
 
+
     public void setFirstImage(String firstImage)
     {
         this.firstImage = firstImage;
+    }
+
+
+    public TargetCharity getCharity()
+    {
+        return charity;
+    }
+
+
+    public void setCharity(TargetCharity charity)
+    {
+        this.charity = charity;
+    }
+
+    public static final class TargetCharity
+    {
+
+        private long id;
+
+        private String image;
+
+
+        public TargetCharity()
+        {
+
+        }
+
+
+        public TargetCharity(Charity charity)
+        {
+            this.id = charity.getId();
+            this.image = charity.getProfilePicture();
+        }
+
+
+        public long getId()
+        {
+            return id;
+        }
+
+
+        public void setId(long id)
+        {
+            this.id = id;
+        }
+
+
+        public String getImage()
+        {
+            return image;
+        }
+
+
+        public void setImage(String image)
+        {
+            this.image = image;
+        }
+
     }
 
 }
